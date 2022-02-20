6cbd9d45f5f994f508dce2155bde8d92e2f24d71
vapor05 <nicholasbocchini@gmail.com>
2022-02-20 12:49:15 -0600

    Added app database, schema, and table ddl script
    Wrote sql scripts for the app's database schema

---
+++ sql/ddl/create_schema.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA financeview;
+
+CREATE TABLE financeview.description (
+    id SERIAL PRIMARY KEY NOT NULL,
+    description TEXT,
+    createdate TIMESTAMP,
+    updatedate TIMESTAMP
+);
+
+CREATE TABLE financeview.expense (
+    id SERIAL PRIMARY KEY NOT NULL,
+    date DATE,
+    description_id INT,
+    amount MONEY,
+    comment TEXT,
+    createdate TIMESTAMP,
+    updatedate TIMESTAMP
+);
+
+CREATE TABLE financeview.category (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT,
+    createdate TIMESTAMP,
+    updatedate TIMESTAMP
+);
+
+CREATE TABLE financeview.expense_category (
+    id SERIAL PRIMARY KEY NOT NULL,
+    expense_id INT NOT NULL,
+    category_id INT NOT NULL
+);
