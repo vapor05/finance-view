392c5af0f7dd8de99b33ff0725772dad845e1ac7
vapor05 <nicholasbocchini@gmail.com>
2022-02-22 21:16:05 -0600

    Replace default http server with a gin http server

--- server.go
+++ server.go
@@ -1,29 +1,51 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
 	"github.com/vapor05/financeview/graph"
 	"github.com/vapor05/financeview/graph/generated"
+	"github.com/vapor05/financeview/pkg/store"
 )
 
-const defaultPort = "8080"
+// Defining the Graphql handler
+func graphqlHandler() (gin.HandlerFunc, error) {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	db, err := store.NewDatabase(context.Background(), os.Getenv("DBURL"))
+	if err != nil {
+		return func(c *gin.Context) {}, fmt.Errorf("failed to connect to database, %w", err)
+	}
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{Db: db}}))
+	ghf := func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+	return ghf, nil
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
+	// Setting up Gin
+	r := gin.Default()
+	gh, err := graphqlHandler()
+	if err != nil {
+		log.Fatalf("failed to setup graphql handler, %v", err)
 	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	r.POST("/query", gh)
+	r.GET("/", playgroundHandler())
+	r.Run()
 }
